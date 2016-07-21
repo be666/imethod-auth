02a7ceb2c274536e6b29c4f158973cb9da1e86e5
bqxu <bcaring@163.com>
2016-07-21 21:15:41 +0800

    Update authapp

--- server/models/auth-user.js
+++ server/models/auth-user.js
@@ -1,6 +1,18 @@
 'use strict';
 let tools = require("../../tools");
 let async = require("async");
+let loopback = require("loopback");
 module.exports = function (AuthUser) {
-  
+
+  AuthUser.me = function (next) {
+    var ctx = loopback.getCurrentContext();
+    var currentUser = ctx && ctx.get('currentUser');
+    next(null, currentUser);
+  };
+
+  AuthUser.remoteMethod("me", {
+    returns: {type: 'Object', root: true},
+    http: {path: "/me", verb: "post"}
+  });
+
 };
